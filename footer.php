13e835f073b9be122c3ca2f7eb7f34ffebca8225
Bryan Racoma <bryanracoma2002@gmail.com>
2025-02-20 14:58:56 +0800

    Footer

--- footer.php
+++ footer.php
@@ -1,5 +1,47 @@
-<p>This is the footer</p>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php wp_footer(); ?>
-</body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Barcode Generator</title>
+    <link rel="stylesheet" href="styles/footer.css">
+
+</head>
+    <footer>
+        <div class="container footer-grid">
+            <div class="brand">
+                <p>CubeTech Innovations © 2024</p>
+                <div class="social-links">
+                    <a href="#" aria-label="Facebook"><i class="fab fa-facebook"></i></a>
+                    <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
+                    <a href="#" aria-label="Instagram"><i class="fab fa-instagram"></i></a>
+                </div>
+                <div class="input-group">
+                    <input type="email" placeholder="Email">
+                    <button class="btn-primary">Subscribe</button>
+                </div>
+            </div>
+            <div class="footer-links">
+                <div>
+                    <h4>Company</h4>
+                    <a href="#">About Us</a>
+                    <a href="#">Privacy Policy</a>
+                    <a href="#">Terms and Conditions</a>
+                </div>
+                <div>
+                    <h4>Products</h4>
+                    <a href="#">SwapServe</a>
+                    <a href="#">Barcode Generator</a>
+                    <a href="#">QR Code Generator</a>
+                </div>
+                <div>
+                    <h4>Services</h4>
+                    <a href="#">Barcode Generator</a>
+                    <a href="#">QR Code Generator</a>
+                    <a href="#">Job Search</a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </html>
