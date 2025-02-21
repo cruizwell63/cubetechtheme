47286ed4ce460c876b430d6bafa78b63cc86c605
Bryan Racoma <bryanracoma2002@gmail.com>
2025-02-21 13:25:07 +0800

    QR CODE

--- src/qr-code-generator-blockone/render.php
+++ src/qr-code-generator-blockone/render.php
@@ -1,17 +1,4 @@
 <div class="container-section">
 	<h1>Online QR Code Generator</h1>
-	<div class="generator-box">
-		<div class="input-group">
-			<input type="text" placeholder="Type your data here...">
-			<button class="btn-primary">></button>
-		</div>
-
-		<div class="barcode-container">
-			<div class="dashed-box"></div>
-			<div class="dashed-box">
-				<img src="barcode.png" alt="Barcode">
-				<button class="btn-primary full-width">DOWNLOAD</button>
-			</div>
-		</div>
-	</div>
+	<div class="generator-box"></div>
 </div>
